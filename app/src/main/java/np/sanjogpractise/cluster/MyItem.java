59a7df2f9854d076135a59073c0d1b9eeb90bc8e
Sanjog Shrestha <sanjog@zyoba.com>
2015-04-06 12:13:04 +0545

    initial commit

---
+++ app/src/main/java/np/sanjogpractise/cluster/MyItem.java
@@ -0,0 +1,17 @@
+package np.sanjogpractise.cluster;
+
+import com.google.android.gms.maps.model.LatLng;
+import com.google.maps.android.clustering.ClusterItem;
+
+public class MyItem implements ClusterItem {
+    private final LatLng mPosition;
+
+    public MyItem(double lat, double lng) {
+        mPosition = new LatLng(lat, lng);
+    }
+
+    @Override
+    public LatLng getPosition() {
+        return mPosition;
+    }
+}
